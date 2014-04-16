e3be0b700673505d83392876e43d95826c8e5bf0
durato <durato@users.noreply.github.com>
2014-04-16 10:08:28 +0200

    added --new-delimiter option

--- grepSilon.py
+++ grepSilon.py
@@ -1,11 +1,13 @@
 import sys
 
+newDelimiter="###GREPSILON###"
+
 def usage():
         print "USAGE:"
-        print sys.argv[0],"<delim>","<match>","<filename>"
+        print sys.argv[0],"<delim>","<match>","--new-delimiter <new delimiter>","<filename>"
         print " "
         print "This script looks for <match> in blocks surrounded by beginning of file, <delim> and EOF"
-        print "Tip: this script generates delimiters for ease of use. (#####################)"
+        print "Tip: this script generates delimiters for ease of use. ("+newDelimiter
 
 def grep(delim, str, file):
         buf=""
@@ -23,16 +25,20 @@ def grep(delim, str, file):
         if found:
                 yield buf
 
+
+
 if len(sys.argv)>3:
         file=None
+	if len(sys.argv) == 6:
+		if sys.argv[3]=="--new-delimiter":
+			newDelimiter=sys.argv[4]
         try:
                 file = open(sys.argv[-1],"r")
         except:
                 print "Error opening",sys.argv[-1]
                 exit()
-        #
         for block in grep(sys.argv[1],sys.argv[2],file):
-                print block,"#####################"
+                print block,newDelimiter
 else:
         usage()
 
